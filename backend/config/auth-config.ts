87eb86db8033faf1e2b7bbf843d05b5a5b8e8a95
AhmedGharrab <=>
2024-05-08 13:15:31 +0100

    setting env variables

--- backend/config/auth-config.ts
+++ backend/config/auth-config.ts
@@ -3,11 +3,11 @@ require('dotenv').config();
 const config = {
     authRequired: false,
     auth0Logout: true,
-    secret: process.env.SECRET,
-    baseURL: process.env.BASEURL,
-    clientID: process.env.CLIENTID,
-    clientSecret: process.env.CLIENTSECRET,
-    issuerBaseURL: process.env.ISSUER_DOMAIN,
+    secret: process.env.secret,
+    baseURL: process.env.AUTH0_AUDIENCE,
+    clientID: process.env.AUTH0_CLIENT_ID,
+    clientSecret: process.env.AUTH0_CLIENT_SECRET,
+    issuerBaseURL: process.env.AUTH0_DOMAIN,
 };
 
 export {config};
--- backend/config/ormconfig.ts
+++ backend/config/ormconfig.ts
@@ -3,15 +3,16 @@ import { DataSource, DataSourceOptions } from "typeorm";
 
 const config = {
     type: 'postgres',
-    host: process.env.PGHOST,
-    port: parseInt(process.env.DBPORT || '5432'),
-    username: process.env.PGUSER,
-    password: process.env.PGPASSWORD,
-    database: process.env.PGDATABASE,
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT || '5432'),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     synchronize: true,
-    logging: process.env.NODE_ENV === "dev" ? false : false,
+    logging: process.env.MODE === "DEV" ? false : false,
     entities: ["entity/*.entity.ts"],
-    migrations: ["migrations/*.ts"]
+    migrations: ["migrations/*.ts"],
+    ssl: process.env.MODE === 'DEV' ? true : false,   
 };
 
 export default { config };
