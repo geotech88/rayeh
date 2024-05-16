fe69dec30d4b61dc0201525b0b28977ea5325225
oussama itou <77784415+th-creator@users.noreply.github.com>
2024-05-16 13:41:15 +0100

    Update index.ts

--- backend/index.ts
+++ backend/index.ts
@@ -62,7 +62,7 @@ AppDataSource.initialize().then(async () => {
     //     });
     // });
 
-    server.listen(process.env.PORT || 3000, () => {
+    app.listen(process.env.PORT || 3000, () => {
         console.log(`Server is running on port ${process.env.PORT || 3000}`);
     });
 }).catch((err) => {
