fd66a1ba1b56fdf23b0c38cd82fdcc68eef2a8e9
ouss.itou1 <ouss.itou1@gmail.com>
2024-05-16 16:07:16 +0100

    Add rewrite-import file for js

--- backend/rewrite-imports.ts
+++ backend/rewrite-imports.ts
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-function rewriteImports(directory) {
+function rewriteImports(directory: any) {
     const files = fs.readdirSync(directory);
 
     for (let file of files) {
