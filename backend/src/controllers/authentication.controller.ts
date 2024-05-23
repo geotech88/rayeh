d800e3b7f64e674fd25052c6dd8a15af749f8145
AhmedGharrab <=>
2024-05-23 14:25:31 +0100

    reviews

--- backend/src/controllers/authentication.controller.ts
+++ backend/src/controllers/authentication.controller.ts
@@ -14,50 +14,52 @@ export class AuthenticationController {
         if (!token) {
             return res.status(401).json({ message: 'Access token is missing or invalid' });
         }
-        // if (!role) {
-        //     return res.status(400).json({message: 'missing parameter!'})
-        // }
 
-      
         try {
-          const response = await fetch(`${process.env.AUTH0_DOMAIN}/userinfo`, {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          });
-          
-          if (!response.ok) {
-            res.status(401).json({ error: 'Invalid token' });
-          }
-          const auth0User = await response.json();
-          const userRepository = AppDataSource.getRepository(User);
-      
-          let user = await userRepository.findOne({ where: { auth0UserId: auth0User.sub } });
-      
-          if (!user) {
-            const newUser = new User();
-            const RoleRepository = AppDataSource.getRepository(Role);
-            const roleUser = new Role();
-            roleUser.name = 'user';
-            await RoleRepository.save(roleUser);
-            newUser.name = auth0User.name as string;
-            newUser.email = auth0User.email as string;
-            newUser.auth0UserId = auth0User.sub as string;
-            newUser.path = auth0User.picture as string;
-            newUser.role = roleUser;
-            await userRepository.save(newUser);
-            await WalletController.createWallet(req, res);
-          }
-      
-          const jwtToken = jwt.sign({ userId: auth0User.sub }, process.env.AUTH0_CLIENT_SECRET!, {
-            algorithm: 'HS256',
-            expiresIn: '24h',
-          });
-      
-          res.status(200).json({ token: jwtToken });
+            const response = await fetch(`${process.env.AUTH0_DOMAIN}/userinfo`, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            });
+
+            if (!response.ok) {
+                return res.status(401).json({ error: 'Invalid token' });
+            }
+            
+            const auth0User = await response.json();
+            const userRepository = AppDataSource.getRepository(User);
+            const roleRepository = AppDataSource.getRepository(Role);
+
+            let user = await userRepository.findOne({ where: { auth0UserId: auth0User.sub } });
+
+            if (!user) {
+                user = new User();
+                user.name = auth0User.name as string;
+                user.email = auth0User.email as string;
+                user.auth0UserId = auth0User.sub as string;
+                user.path = auth0User.picture as string;
+
+                let roleUser = await roleRepository.findOne({ where: { name: 'user' } });
+                if (!roleUser) {
+                    roleUser = new Role();
+                    roleUser.name = 'user';
+                    await roleRepository.save(roleUser);
+                }
+
+                user.role = roleUser;
+                await userRepository.save(user);
+                await WalletController.createWallet(req, res);
+            }
+
+            const jwtToken = jwt.sign({ userId: auth0User.sub }, process.env.AUTH0_CLIENT_SECRET!, {
+                algorithm: 'HS256',
+                expiresIn: '24h',
+            });
+
+            return res.status(200).json({ token: jwtToken });
         } catch (error: any) {
-          console.error('Error authenticating user:', error);
-          res.status(401).json({ error: error.message });
+            console.error('Error authenticating user:', error);
+            return res.status(500).json({ error: 'Internal Server Error' });
         }
-      };
+    };
 }
