624ff7333b308203e55e975d449ff92c64acd140
AhmedGharrab <=>
2024-05-27 17:32:55 +0100

    reviews

--- backend/src/types/custom-types.d.ts
+++ backend/src/types/custom-types.d.ts
@@ -1,4 +1,10 @@
-declare module 'multer' {
-  const multer: any;
-  export default multer;
+// custom-types.d.ts
+import { File } from 'multer';
+
+declare global {
+  namespace Express {
+    interface Request {
+      file?: File; // This is optional; use file!: File; if it should be non-nullable
+    }
+  }
 }
